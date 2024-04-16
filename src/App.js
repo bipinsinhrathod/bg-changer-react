dfdd459ab6cd5bafc090881ee74e56152d36233a
bipinsinhrathod <bipinrajput548@gmail.com>
2024-04-16 12:37:26 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
             style={{ backgroundColor: "green" }}
           >
             Green
+            
           </button>
           <button
             onClick={() => setColor("blue")}
